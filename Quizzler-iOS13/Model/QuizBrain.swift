493adc36db7ef228556a57953fc8a1134aa2af95
StefDev <stefhanytss@gmail.com>
2022-11-07 21:18:44 -0300

    Quiz
    Aplicativo de Quiz, com intuito de consolidar os conhecimentos de POO, Classes, Heranças, Encapsulamento, Protocols entre outros conhecimentos.

--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -14,18 +14,18 @@ struct QuizBrain {
     var score = 0
     
     let quiz = [
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-        Question(q: "Google was originally called 'Backrub'.", a: "True"),
-        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        Question(q: "O sangue de uma lesma é verde.", a: "True"),
+        Question(q: "Aproximadamente um quarto dos ossos humanos estão nos pés.", a: "True"),
+        Question(q: "A área total da superfície de dois pulmões humanos é de aproximadamente 70 metros quadrados.", a: "True"),
+        Question(q: "Em West Virginia, EUA, se você acidentalmente bater em um animal com seu carro, você pode levá-lo para casa para comer.", a: "True"),
+        Question(q: "Em Londres, no Reino Unido, se você morrer na Câmara do Parlamento, tecnicamente tem direito a um funeral de estado, porque o prédio é considerado um lugar sagrado demais.", a: "False"),
+        Question(q: "É ilegal fazer xixi no oceano em Portugal.", a: "True"),
+        Question(q: "Você pode levar uma vaca escada abaixo, mas não subir escadas.", a: "False"),
+        Question(q: "O Google foi originalmente chamado de 'Backrub'.", a: "True"),
+        Question(q: "O nome de solteira da mãe de Buzz Aldrin era 'Moon'.", a: "True"),
+        Question(q:"O som mais alto produzido por qualquer animal é de 188 decibéis. Esse animal é o elefante africano.", a: "False"),
+        Question(q:"Nenhum pedaço de papel quadrado seco pode ser dobrado ao meio mais de 7 vezes.", a: "False"),
+        Question(q:"Chocolate afeta o coração e o sistema nervoso de um cachorro; alguns gramas são suficientes para matar um cachorro pequeno.", a: "True")
     ]
     
     func getQuestionText() -> String {
@@ -46,6 +46,7 @@ struct QuizBrain {
             questionNumber += 1
         } else {
             questionNumber = 0
+            
         }
     }
     
